250399c22cbff23ec3eaaba81b4f937fb95121a2
cjvidro <cjvidro@mtu.edu>
2020-02-17 16:56:44 -0500

    Initial commit

---
+++ src/Test/MainTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void start() {
+        assertEquals(2, 2);
+    }
+}
