6f6d897bb90647d0e63f577ed5391d4a34b6ab92
cjvidro <cjvidro@mtu.edu>
2020-03-16 13:03:01 -0400

    Setup addPlaysound buttons

--- src/jSONEditor/ViewProjectController.java
+++ src/jSONEditor/ViewProjectController.java
@@ -1,11 +1,11 @@
 package jSONEditor;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,17 +14,19 @@ import java.io.IOException;
 public class ViewProjectController {
 
     /*****************************************************
-     * FXML Elements
+     * Change Scenes and Button Functionality
      *****************************************************/
+    @FXML
+    private void quit(ActionEvent event) throws IOException {
+        System.out.println("Quit");
 
-    @FXML // Needed by FXML to initialize anything
-    private void initialize() {
-        // Perform any initializations here
+        /*
+        INSERT Prompt Save FUNCTIONALITY HERE
+         */
+
+        System.exit(0);
     }
 
-    /*****************************************************
-     * Change Scenes
-     *****************************************************/
     @FXML
     private void showExport(ActionEvent event) throws IOException {
         System.out.println("Show Export");
@@ -43,10 +45,6 @@ public class ViewProjectController {
         exportWindow.setResizable(false);
         exportWindow.show();
     }
-
-    /*****************************************************
-     * Change Scenes
-     *****************************************************/
 
     @FXML
     private void showSettings(ActionEvent event) throws IOException {
@@ -103,5 +101,70 @@ public class ViewProjectController {
         addTemplateWindow.initModality(Modality.APPLICATION_MODAL);
         addTemplateWindow.setResizable(false);
         addTemplateWindow.show();
+    }
+
+    @FXML
+    private void showAddPlaysound(ActionEvent event) throws IOException {
+        System.out.println("Show Add Playsound");
+
+        // load FXML and set the controller
+        ViewProjectController controller = new ViewProjectController(); // the controller for the view project GUI
+        FXMLLoader loader = new FXMLLoader((getClass().getResource("../view/addPlaysound.fxml")));
+        loader.setController(controller); // addPlaysound/viewProject controller
+        Parent root = loader.load();
+
+        Stage addPlaysoundWindow = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+        double width =  ((Button) event.getSource()).getScene().getWidth();
+        double height = ((Button) event.getSource()).getScene().getHeight();
+
+        // set JavaFX stage details
+        addPlaysoundWindow.setScene(new Scene(root, width, height));
+    }
+
+    @FXML
+    private void saveAddPlaysound(ActionEvent event) throws IOException {
+        System.out.println("Save Add Playsound");
+
+        // load FXML and set the controller
+        ViewProjectController controller = new ViewProjectController(); // the controller for the view project GUI
+        FXMLLoader loader = new FXMLLoader((getClass().getResource("../view/viewProject.fxml")));
+        loader.setController(controller); // addPlaysound/viewProject controller
+        Parent root = loader.load();
+
+        /*
+        INSERT Save FUNCTIONALITY HERE
+         */
+
+        Stage viewProjectWindow = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+        double width =  ((Button) event.getSource()).getScene().getWidth();
+        double height = ((Button) event.getSource()).getScene().getHeight();
+
+        // set JavaFX stage details
+        viewProjectWindow.setScene(new Scene(root, width, height));
+    }
+
+    @FXML
+    private void cancelAddPlaysound(ActionEvent event) throws IOException {
+        System.out.println("Cancel Add Playsound");
+
+        // load FXML and set the controller
+        ViewProjectController controller = new ViewProjectController(); // the controller for the view project GUI
+        FXMLLoader loader = new FXMLLoader((getClass().getResource("../view/viewProject.fxml")));
+        loader.setController(controller); // addPlaysound/viewProject controller
+        Parent root = loader.load();
+
+        /*
+        INSERT Cancel FUNCTIONALITY HERE
+         */
+
+        Stage viewProjectWindow = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+        double width =  ((Button) event.getSource()).getScene().getWidth();
+        double height = ((Button) event.getSource()).getScene().getHeight();
+
+        // set JavaFX stage details
+        viewProjectWindow.setScene(new Scene(root, width, height));
     }
 }
