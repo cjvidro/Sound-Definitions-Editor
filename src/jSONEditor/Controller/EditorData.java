a9be16ab91eba5722110e130afee12e4c374583c
cjvidro <cjvidro@mtu.edu>
2020-04-17 15:59:19 -0400

    Fixed config save bug

--- src/jSONEditor/Controller/EditorData.java
+++ src/jSONEditor/Controller/EditorData.java
@@ -55,6 +55,8 @@ public class EditorData {
 
     public static boolean serializeSaves() {
         try {
+            File file = new File("./config");
+            file.mkdir();
             FileOutputStream fos = new FileOutputStream("./config/saves.ser");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(saves);
