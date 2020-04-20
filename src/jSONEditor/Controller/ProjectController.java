775f3eae951b383035255f97748384d460182fb3
cjvidro <cjvidro@mtu.edu>
2020-04-19 20:22:58 -0400

    View Project CSS

--- src/jSONEditor/Controller/ProjectController.java
+++ src/jSONEditor/Controller/ProjectController.java
@@ -112,10 +112,6 @@ public class ProjectController {
          * INSERT POPULATE TEMPLATES
          */
 
-        /*
-         * CALL READ SOUND DEFINITIONS
-         */
-
         // Populate the playsounds on the LHS
         populatePlaysounds();
     }
